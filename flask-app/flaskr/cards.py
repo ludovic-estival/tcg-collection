1efdc445dd6d36a91565001984434abf04e2228d
Ludovic Estival <ludovic.estival@etu.univ-tlse2.fr>
2024-01-31 13:16:13 +0100

    Add create form and add style

--- flask-app/flaskr/cards.py
+++ flask-app/flaskr/cards.py
@@ -13,3 +13,31 @@ def view_cards():
     db = get_db()
     cards = db.execute('SELECT * FROM card ORDER BY name ASC').fetchall()
     return render_template('index.html', cards=cards, count=len(cards))
+
+
+@bp.route('/create', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        code = request.form['code']
+        name = request.form['name']
+        rarity = request.form['rarity']
+        price = request.form['price']
+        db = get_db()
+
+        if not price:
+            db.execute(
+                'INSERT INTO card (code, rarity, name)'
+                ' VALUES (?, ?, ?)',
+                (code, rarity, name)
+            )
+        else:
+            db.execute(
+                'INSERT INTO card (code, rarity, name, price)'
+                ' VALUES (?, ?, ?, ?)',
+                (code, rarity, name, price)
+            )
+            
+        db.commit()
+        return redirect('/')
+
+    return render_template('create.html')
